6acd07fd20dbf536a3f8fd5a54b9c53c6efcd88d
vova <vova@debian>
2014-12-14 11:12:49 +0300

    develop

--- stars_all.py
+++ stars_all.py
@@ -10,6 +10,26 @@ from matplotlib.patches import Circle, Arc
 from mpl_toolkits.mplot3d import Axes3D 
 import mpl_toolkits.mplot3d.art3d as art3d
 
+#Column	HR{1}	(I4)	[1/9110]+ "Bright Star" catalog <V/50> [NULL integer written as an empty string]	[ucd=]
+#Column	HD{1}	(I6)	[1/225300] Henry Draper" catalog <III/135> [NULL integer written as an empty string]	[ucd=]
+#Column	GLON{1}	(F6.2)	Galactic longitude	[ucd=]
+#Column	GLAT{1}	(F6.2)	Galactic latitude	[ucd=]
+#Column	Dist{1}	(I5)	Distance of star in light-years (5) [NULL integer written as an empty string]	[ucd=]
+#Column	Vmag{1}	(F5.2)	[-1.46/5.03] Visual magnitude (magnitude visuelle)	[ucd=]
+#Column	Name{2}	(a12)	Common name of the star / Nom usuel	[ucd=]
+
+
+#data = np.loadtxt('data/stars4.tsv', skiprows=50, delimiter='|', usecols=(1, 2, 3, 4, 5, 6),
+#                  dtype=[('hd', 'int'), ('glong', 'float'), ('glat', 'float'), ('distance', 'float'), ('vmag', 'float'),('name', 'S20')])
+
+
+#data = np.loadtxt('data/stars5.tsv', skiprows=50, delimiter='|', usecols=(2, 4),#usecols=(0, 1, 2, 3, 4),
+                  #dtype=[('glong', 'float'), ('glat', 'float'), ('vmag', 'float'), ('parallax', 'float'), ('hd', 'int')])
+#                  dtype=[('vmag', 'float'), ('hd', 'int')])
+
+
+#================================================================================================
+
 
 def set_graph_title(s):
     plt.title(s)
@@ -72,28 +92,138 @@ def parsec_to_lightyear(dist):
     return dist * LIGHT_YEARS_IN_PARSEC
 
 
+v53a=1
+hipparcos=2
+yale=3
+
+use_catalog=hipparcos
+
+if use_catalog == v53a:
+#========================================= load 1 ============================================================
+
+	print "cat"
+
+#	data = np.loadtxt('data/v53a_5.tsv', skiprows=50, delimiter='|', usecols=(1, 2, 3, 4, 5, 6),
+#                  dtype=[('hd', 'int'), ('glong', 'float'), ('glat', 'float'), ('dist_lyr', 'float'), ('vmag', 'float'),('name', 'S20')])
+
+#HR|HD|Const|GLON|GLAT|Dist|Vmag|Name
+
+	data = np.loadtxt('data/v53a_6.tsv', skiprows=60, delimiter='|', usecols=(1, 2, 3, 4, 5, 6,7 ),
+                  dtype=[('hd', 'int'), ('con','S20'), ('glong', 'float'), ('glat', 'float'), ('dist_lyr', 'float'), ('vmag', 'float'),('name', 'S20')])
 
 
-dt = np.loadtxt('data/stars5.tsv', skiprows=50, delimiter='|', usecols=(0, 1, 2, 3, 4),
-                  dtype=[('glong', 'float'), ('glat', 'float'), ('vmag', 'float'), ('parallax', 'float'), ('hd', 'int')])
+	data = np.sort(data, order=['vmag'])
 
-names = np.loadtxt('data/names1.tsv', skiprows=47, delimiter='|', usecols=(0, 1), dtype=[('hd', 'int'), ('name', 'S20')])
+	#data = data[0:29]
 
-dt = rfn.join_by('hd',dt,names,jointype='leftouter',defaults={'name':'123'})
+	dt=data
 
 
-dt = dt[dt["parallax"] != 0]
 
+#getting most distant visible star
+#https://ru.wikipedia.org/wiki/VV_%D0%A6%D0%B5%D1%84%D0%B5%D1%8F
+#HD 208816
+#http://simbad.u-strasbg.fr/simbad/sim-id?Ident=VV_Cep
+#histogram - distance to visible stars
+#	dt=dt[dt["vmag"]<6.5]
+
+#	dt=dt[dt["dist_lyr"]<300]
+#	plt.hist(dt['dist_lyr'],bins=150)
+#	plt.show()
+
+#	dt = np.sort(dt,order=['dist_lyr'])
+
+#	print dt.size
+#	print dt
+
+	#print "max visible star dist lyr: " + dt.max("dist_lyr") #str(np.amax(dt["dist_lyr"]))
+
+#	exit()
+
+
+
+#========================================= load 1 ============================================================
+
+
+#========================================= load 2 ============================================================
+elif use_catalog== hipparcos:
+	print "hipparcos"
+
+	data = np.loadtxt('data/stars5.tsv', skiprows=50, delimiter='|', usecols=(0, 1, 2, 3, 4),
+        	          dtype=[('glong', 'float'), ('glat', 'float'), ('vmag', 'float'), ('parallax', 'float'), ('hd', 'int')])
+
+	names = np.loadtxt('data/names1.tsv', skiprows=47, delimiter='|', usecols=(0, 1), dtype=[('hd', 'int'), ('name', 'S20')])
+
+	dt = rfn.join_by('hd',data,names,jointype='leftouter',defaults={'name':'123'})
+
+	dt=np.sort(dt, order=['vmag'])
+
+	print data[data["parallax"]==0]
+
+	dt = dt[dt["parallax"]!=0]
+	#dt = dt[0:99]
 
 #	rfn.append_fields(dt, "dist", dtypes='S20')
-dt = add_field(dt, [('dist', float)])
-dt = add_field(dt, [('dist_lyr', float)])
+	dt = add_field(dt, [('dist', float)])
+	dt = add_field(dt, [('dist_lyr', float)])
 
-dt["dist"] = 1/(dt["parallax"]/1000.0)
-dt["dist_lyr"] = parsec_to_lightyear(dt["dist"])
+	dt["dist"]=1/(dt["parallax"]/1000.0)
+	dt["dist_lyr"]=parsec_to_lightyear(dt["dist"])
 
 
 
+
+#	dt=dt[dt["vmag"]<6.5]
+
+#	dt = np.sort(dt,order=['dist_lyr'])
+
+#	print dt.size
+#	print dt
+
+	#print "max visible star dist lyr: " + dt.max("dist_lyr") #str(np.amax(dt["dist_lyr"]))
+
+#	exit()
+
+#========================================= load 2 ============================================================
+
+
+
+#========================================= load yale ============================================================
+elif use_catalog==yale:
+	print "yale"
+
+	data = np.loadtxt('data/yale_i50_2.tsv', skiprows=45, delimiter='|', usecols=(0, 1, 2, 3, 4),
+        	          dtype=[('hd', 'int'), ('glong', 'float'), ('glat', 'float'), ('vmag', 'float'), ('parallax', 'float') ])
+#				converters = {0:lambda s: s, 4: lambda s: float(s or 0)})
+
+	names = np.loadtxt('data/names1.tsv', skiprows=47, delimiter='|', usecols=(0, 1), dtype=[('hd', 'int'), ('name', 'S20')])
+
+	dt = rfn.join_by('hd',data,names,jointype='leftouter',defaults={'name':'123'})
+
+	dt=np.sort(dt, order=['vmag'])
+
+#	print data[data["parallax"]==0]
+
+	dt = dt[dt["parallax"]!=0]
+	#dt = dt[0:19]
+
+	dt = add_field(dt, [('dist', float)])
+	dt = add_field(dt, [('dist_lyr', float)])
+
+	dt["parallax"]=np.abs(dt["parallax"])
+
+	dt["dist"]=1/(dt["parallax"])
+	dt["dist_lyr"]=parsec_to_lightyear(dt["dist"])
+	
+	print dt
+
+#========================================= load yale ============================================================
+
+
+
+#np.append(dt,)
+
+#exit()
 
 #Ursa major
 #dt=dt[ dt["con"] == "UMa"]
@@ -110,15 +240,9 @@ dt["dist_lyr"] = parsec_to_lightyear(dt["dist"])
 
 
 
-
-
-#rfn.append_fields(dt,'x',dtypes='float')
-
 dt=add_field(dt,[('x', float)])
 dt=add_field(dt,[('y', float)])
 dt=add_field(dt,[('z', float)])
-
-
 
 
 #print dt
@@ -135,11 +259,9 @@ dt["z"] = dt["dist_lyr"] * np.sin(dt["glat"])
 
 
 
-polaris = dt[dt["hd"] == 8890]
+polaris = dt[dt["hd"]==8890]
 
-
-
-dt=np.sort(dt, order=['vmag'])
+#print polaris
 
 dt = dt[0:29]
 
@@ -152,7 +274,29 @@ center_y=1000 * np.cos(0) * np.sin(0)
 center_z=1000 * np.sin(0)
 
 
+p1_x=1000 * np.cos(0) * np.cos(np.radians(45))
+p1_y=1000 * np.cos(0) * np.sin(np.radians(45))
+p1_z=1000 * np.sin(0)
 
+p2_x=1000 * np.cos(0) * np.cos(np.radians(315))
+p2_y=1000 * np.cos(0) * np.sin(np.radians(315))
+p2_z=1000 * np.sin(0)
+
+
+p3_x=1000 * np.cos(0) * np.cos(np.radians(135))
+p3_y=1000 * np.cos(0) * np.sin(np.radians(135))
+p3_z=1000 * np.sin(0)
+
+p4_x=1000 * np.cos(0) * np.cos(np.radians(225))
+p4_y=1000 * np.cos(0) * np.sin(np.radians(225))
+p4_z=1000 * np.sin(0)
+
+
+
+#================================= Galaxies near Milky Way ======================================
+
+
+#================================= Galaxies near Milky Way ======================================
 
 def show_galaxies_near_milky_way():
 
